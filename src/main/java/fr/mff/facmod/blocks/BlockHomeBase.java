27587d405a2ca2f4203484b317e4696e29695533
Moscato42 <mosca42390@gmail.com>
2016-07-07 19:24:51 +0200

    Add 2 function

--- src/main/java/fr/mff/facmod/blocks/BlockHomeBase.java
+++ src/main/java/fr/mff/facmod/blocks/BlockHomeBase.java
@@ -3,16 +3,28 @@ package fr.mff.facmod.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+
+import java.util.Random;
+
 import fr.mff.facmod.FactionMod;
 
 public class BlockHomeBase extends Block {
 
 	public BlockHomeBase(Material material) {
 		super(material);
-		this.setHardness(100.F);
+		this.setHardness(60.F);
 		this.setResistance(1000.F);
 		this.setLightLevel(1.0F);
 		this.setCreativeTab(FactionMod.factionTabs);
 	}
 
+	
+    public int quantityDropped(Random random)
+    {
+        return 0;
+    }
+    protected boolean canSilkHarvest()
+    {
+        return false;
+    }
 }
