809ead7129262765e3926eacff8880631538a502
MFFbrokenSwing <allocompte@hotmail.fr>
2016-07-28 17:23:35 +0200

    This file was unchecked, uups

--- src/main/java/fr/mff/facmod/client/overlay/Overlays.java
+++ src/main/java/fr/mff/facmod/client/overlay/Overlays.java
@@ -1,5 +1,7 @@
 package fr.mff.facmod.client.overlay;
 
+import java.util.List;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
@@ -12,6 +14,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.lwjgl.opengl.GL11;
 
+import com.google.common.collect.Lists;
+
 import fr.mff.facmod.FactionMod;
 import fr.mff.facmod.config.ConfigFaction;
 import fr.mff.facmod.core.EnumRank;
@@ -21,39 +25,60 @@ import fr.mff.facmod.proxy.ClientProxy;
 public class Overlays {
 
 	public static final ResourceLocation BANNER = new ResourceLocation(FactionMod.MODID, "textures/overlay/banner.png");
+	public static final ResourceLocation PANEL = new ResourceLocation(FactionMod.MODID, "textures/overlay/panel.png");
 	public static float alpha = 1.0f;
 
 	public static void onRenderGameOverlay(RenderGameOverlayEvent.Text e) {
-		int i = 0;
-		if(ConfigFaction.FACTION_OVERLAY) {
-			String factionName = ((ClientProxy)FactionMod.proxy).factionName;
-			if(!factionName.isEmpty()) {
-				String str = EnumChatFormatting.UNDERLINE + I18n.format("overlay.faction", new Object[0]) + "§r : " + EnumChatFormatting.GOLD + factionName;
-				Minecraft.getMinecraft().fontRendererObj.drawString(str, 10, 10 + i * 15, 0xFFFFFF);
-				i++;
+		renderFactionPanel();
+		renderBanner();
+	}
+
+	public static void renderFactionPanel() {
+		String factionName = ((ClientProxy)FactionMod.proxy).factionName;
+		if(!factionName.isEmpty() || ConfigFaction.POWER_OVERLAY) {
+			
+			List<String> toPrint = Lists.newArrayList();
+			if(ConfigFaction.FACTION_OVERLAY) {
+				toPrint.add(EnumChatFormatting.GOLD + factionName);
 			}
-		}
-		if(ConfigFaction.RANK_OVERLAY) {
-			EnumRank rank = ((ClientProxy)FactionMod.proxy).rank;
-			if(rank != EnumRank.WITHOUT_FACTION) {
-				String str = EnumChatFormatting.UNDERLINE + I18n.format("overlay.rank", new Object[0]) + "§r : " + rank.getColor() + rank.getDisplay();
-				Minecraft.getMinecraft().fontRendererObj.drawString(str, 10, 10 + i * 15, 0xFFFFFF);
-				i++;
+			if(ConfigFaction.RANK_OVERLAY) {
+				EnumRank rank = ((ClientProxy)FactionMod.proxy).rank;
+				toPrint.add(rank.getColor() + rank.getDisplay());
 			}
-		}
-		if(ConfigFaction.POWER_OVERLAY) {
-			String powerLevel = ((ClientProxy)FactionMod.proxy).powerLevel;
-			String str = EnumChatFormatting.UNDERLINE + I18n.format("overlay.power", new Object[0]) + "§r : " + powerLevel;
-			Minecraft.getMinecraft().fontRendererObj.drawString(str, 10, 10 + i * 15, 0xFFFFFF);
-			i++;
-		}
-		if(ConfigFaction.LAND_OWNER_OVERLAY) {
-			renderBanner();
+			if(ConfigFaction.POWER_OVERLAY) {
+				toPrint.add(I18n.format("overlay.power", new Object[0]) + " : " + ((ClientProxy)FactionMod.proxy).powerLevel);
+			}
+			int maxlength = 0;
+			for(String s : toPrint) {
+				int length = Minecraft.getMinecraft().fontRendererObj.getStringWidth(s);
+				if(length > maxlength) {
+					maxlength = length;
+				}
+			}
+			float xScale = maxlength / 89.0F;
+			Minecraft.getMinecraft().getTextureManager().bindTexture(PANEL);
+			GL11.glPushMatrix();
+			GL11.glScalef(xScale, 1.0f, 1.0f);
+			
+			int infoCount = 0;
+			Gui.drawScaledCustomSizeModalRect(0, 0, 0, 0, 100, 7, 110, 8, 100, 40);
+			for(int i = 0; i < toPrint.size(); i++) {
+				Gui.drawScaledCustomSizeModalRect(0, 29 * infoCount + 7, 0, 7, 100, 26, 110, 29, 100, 40);
+				infoCount++;
+			}
+			Gui.drawScaledCustomSizeModalRect(0, 29 * infoCount + 7, 0, 33, 100, 7, 110, 8, 100, 40);
+			GL11.glPopMatrix();
+			
+			infoCount = 0;
+			for(String s : toPrint) {
+				Minecraft.getMinecraft().fontRendererObj.drawString(s, (int)(55 * xScale) - (Minecraft.getMinecraft().fontRendererObj.getStringWidth(s) / 2), 18 + infoCount * 29, 0xFFFFFF);
+				infoCount++;
+			}
 		}
 	}
 
 	public static void renderBanner() {
-		if(alpha > 0) {
+		if(ConfigFaction.LAND_OWNER_OVERLAY && alpha > 0) {
 			GL11.glPushMatrix();
 			String factionName = getLandOwnerAndSetColor();
 			int x = new ScaledResolution(Minecraft.getMinecraft()).getScaledWidth() / 2;
@@ -77,7 +102,7 @@ public class Overlays {
 	private static String getLandOwnerAndSetColor() {
 		String factionName = ((ClientProxy)FactionMod.proxy).landOwner;
 		if(factionName.equalsIgnoreCase("safezone")) {
-			GL11.glColor4f(1.0f, 0.7f, 0.0f, alpha);
+			GL11.glColor4f(1.0f, 1.0f, 0.0f, alpha);
 			return I18n.format("overlay.safeZone", new Object[0]);
 		} else if(factionName.equalsIgnoreCase("warzone")) {
 			GL11.glColor4f(1.0f, 0.0f, 0.0f, alpha);
@@ -86,7 +111,7 @@ public class Overlays {
 			GL11.glColor4f(1.0f, 1.0f, 1.0f, alpha);
 			return factionName;
 		}
-		GL11.glColor4f(1.0f, 0.0f, 0.6f, alpha);
+		GL11.glColor4f(1.0f, 0.0f, 1.0f, alpha);
 		return I18n.format("overlay.freeLand", new Object[0]);
 	}
 
